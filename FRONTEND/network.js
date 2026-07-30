1d1ba1c041f747d0d4b4e1e28717dc121824241d
yashkumar1907 <yashkumar9926@gmail.com>
2026-07-30 16:46:34 +0530

    First three completed

--- FRONTEND/network.js
+++ FRONTEND/network.js
@@ -206,7 +206,7 @@ function formatDate(dateString) {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
-    return `${day}-${month}-${year}`;
+    return `${day}.${month}.${year}`;
 }
 
 
