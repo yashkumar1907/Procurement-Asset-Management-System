cc42bc9801707d6553ab7b830217501fcfbf4d9f
yashkumar1907 <yashkumar9926@gmail.com>
2026-07-31 00:28:21 +0530

    ee

--- BACKEND/services/emailService.js
+++ BACKEND/services/emailService.js
@@ -1,7 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
@@ -17,7 +23,7 @@ async function sendEmail(to, subject, html) {
         console.log("Inside sendEmail");
         console.log("EMAIL_USER:", process.env.EMAIL_USER);
         console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-        
+
         await transporter.sendMail({
             from: process.env.EMAIL_USER,
             to,
