2ab2e93918270fcc739f99aab27bd126403c39d4
yashkumar1907 <yashkumar9926@gmail.com>
2026-07-31 00:01:47 +0530

    lp

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -121,7 +121,7 @@ connectDB();
 const PORT = process.env.PORT;
 
 
-cron.schedule("0 10,16 * * *", async () => {
+cron.schedule("5 0 * * *", async () => {
    console.log("Running PO Reminder Job...");
    await checkNetworkPOReminders();
    await checkAmcPOReminders();
