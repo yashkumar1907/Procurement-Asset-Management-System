642692fcb877ee58e55f9ac652553c3b82c649ab
yashkumar1907 <yashkumar9926@gmail.com>
2026-07-31 00:24:44 +0530

    qw

--- BACKEND/services/emailService.js
+++ BACKEND/services/emailService.js
@@ -14,6 +14,10 @@ const transporter = nodemailer.createTransport({
 // ===============================
 async function sendEmail(to, subject, html) {
     try {
+        console.log("Inside sendEmail");
+        console.log("EMAIL_USER:", process.env.EMAIL_USER);
+        console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+        
         await transporter.sendMail({
             from: process.env.EMAIL_USER,
             to,
